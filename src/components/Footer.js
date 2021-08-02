0423032fa15402d61abad950d648363f1de0e6d6
zzkevin <realzzkevin@gmail.com>
2021-08-02 01:25:08 -0400

    add fontawsome icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer () {
     return (
@@ -7,22 +10,22 @@ function Footer () {
                 <div className="row align-items-center justify-content-between" id="contact-list">
                     <div className="col-3 text-center">
                         <a href="tel:+19175356885" data-bs-toggle="tooltip" data-bs-placement="top" title="917-535-6885">
-                             <span className="fas fa-phone-square-alt fa-3x"></span> 
+                             <span> <FontAwesomeIcon icon={faPhoneSquare} size='3x' /></span> 
                         </a>
                     </div>
                     <div className="col-3 text-center">
                         <a href="mailto:realzzkevin@gmail.com" data-bs-toggle="tooltip" data-bs-placement="top" title="realzzkevin@gmail.com">
-                             <span className="far fa-envelope fa-3x"></span> 
+                             <span><FontAwesomeIcon icon={faEnvelope} size="3x" /></span> 
                         </a>
                     </div>
                     <div className="col-3 text-center">
                         <a href="https://github.com/realzzkevin/" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="to my github page" rel="noreferrer">
-                             <span className="fab fa-github-square fa-3x"></span> 
+                             <span><FontAwesomeIcon icon={faGithubSquare} size="3x" /></span> 
                         </a>
                     </div>
                     <div className="col-3 text-center">
                         <a href="https://www.linkedin.com/in/zhong-zheng-782884210/" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="to my Linkedin page" rel="noreferrer" >
-                             <span className="fab fa-linkedin fa-3x"></span> 
+                             <span><FontAwesomeIcon icon={faLinkedin} size="3x" /></span> 
                         </a>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 
 ReactDOM.render(
     <App />,
